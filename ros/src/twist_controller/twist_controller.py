4e02f6ddd10ee4006a336dc8844773a068cfb63a
Niklas <beck.niklas@gmail.com>
2018-04-19 13:18:14 -0700

    call yaw_controller.py to get steering angle while still going full speed

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -16,25 +16,41 @@ class Controller(object):
         # TODO: Implement
         self.throttle = 1.
         self.brake = 0.
-        self.steer = 0.
 
-        # min_speed = 0.1
-        # self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
-        # kp = 0.3
-        # ki = 0.1
-        # kd = 0.
-        # mn_throttle = 0.
-        # mx_throttle = 0.2 # limited by accel limit
-        # self.throttle_controller = PID(kp, ki, kd, mn_throttle, mx_throttle)
+        min_speed = 0.1
+        self.yaw_controller = YawController(wheel_base, steer_ratio, min_speed, max_lat_accel, max_steer_angle)
 
-        # tau = 0.5 #cutoff frequency = 1/(2*pi*tau)
-        # ts = 0.02 # sample time
-        # self.vel_lpf
+        kp = 0.3
+        ki = 0.1
+        kd = 0.
+        mn_throttle = 0.
+        mx_throttle = 0.2 # limited by accel limit
+        self.throttle_controller = PID(kp, ki, kd, mn_throttle, mx_throttle)
+
+        tau = 0.5 # cutoff frequency = 1/(2*pi*tau)
+        ts = 0.02 # sample time
+        self.vel_lpf = LowPassFilter(tau, ts)
+
+        self.vehicle_mass=vehicle_mass
+        self.fuel_capacity=fuel_capacity
+        self.brake_deadband=brake_deadband
+        self.decel_limit=decel_limit
+        self.accel_limit=accel_limit
+        self.wheel_radius=wheel_radius
+
+        self.last_time = rospy.get_time()
 
     def control(self, current_vel, dbw_enabled, linear_vel, angular_vel):
         # TODO: Change the arg, kwarg list to suit your needs
         # Return throttle, brake, steer
+
+        if not dbw_enabled:
+        	self.throttle_controller.reset()
+        	return 0., 0., 0.
+
+        # run lowpass filter on current velocity
+        current_vel = self.vel_lpf.filt(current_vel)
 
         # throttle and brake
         # use PID controller pid.py
@@ -42,6 +58,7 @@ class Controller(object):
         # steer
         # use yaw controller
 
+        self.steer = self.yaw_controller.get_steering(linear_vel, angular_vel, current_vel)
 
 
         return self.throttle, self.brake, self.steer
